5051d03b168ccc3c49266ed505be76d4c715481f
Nick Owen <nowen@scottlogic.co.uk>
2017-05-05 15:43:50 +0100

    feat: return whole name of customer

--- ShoppingApp/Models/Customer.cs
+++ ShoppingApp/Models/Customer.cs
@@ -1,14 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace ShoppingApp.Models
 {
     public class Customer
     {
         public virtual int Id { get; set; }
+
+        [JsonIgnore]
         public virtual string FirstName { get; set; }
+
+        [JsonIgnore]
         public virtual string LastName { get; set; }
+
         public virtual string EmailAddress { get; set; }
 
         public virtual IList<Purchase> Purchases { get; set; }
+
+        public string Name
+        {
+            get { return $"{this.FirstName} {this.LastName}"; }
+        }
     }
 }

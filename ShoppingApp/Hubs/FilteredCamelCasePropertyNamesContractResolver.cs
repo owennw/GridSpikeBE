71f910357ba23dc6de586dc86fc5946e2f6db1a1
Nick Owen <nowen@scottlogic.co.uk>
2017-05-09 00:55:01 +0100

    fix: change pascal case -> camel case mapping back to UI

---
+++ ShoppingApp/Hubs/FilteredCamelCasePropertyNamesContractResolver.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ShoppingApp.Hubs
+{
+    public class FilteredCamelCasePropertyNamesContractResolver : DefaultContractResolver
+    {
+        public FilteredCamelCasePropertyNamesContractResolver()
+        {
+            AssembliesToInclude = new HashSet<Assembly>();
+            TypesToInclude = new HashSet<Type>();
+        }
+
+        // Identifies assemblies to include in camel-casing
+        public HashSet<Assembly> AssembliesToInclude { get; set; }
+
+        // Identifies types to include in camel-casing
+        public HashSet<Type> TypesToInclude { get; set; }
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var jsonProperty = base.CreateProperty(member, memberSerialization);
+            Type declaringType = member.DeclaringType;
+            if (TypesToInclude.Contains(declaringType) || AssembliesToInclude.Contains(declaringType.Assembly))
+            {
+                jsonProperty.PropertyName = ToCamelCase(jsonProperty.PropertyName);
+            }
+
+            return jsonProperty;
+        }
+
+        private static string ToCamelCase(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            var firstChar = value[0];
+
+            if (char.IsLower(firstChar))
+            {
+                return value;
+            }
+
+            firstChar = char.ToLowerInvariant(firstChar);
+            return firstChar + value.Substring(1);
+        }
+    }
+}
--- ShoppingApp/Startup.cs
+++ ShoppingApp/Startup.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.Owin.Cors;
+using Newtonsoft.Json;
 using Owin;
+using ShoppingApp.Hubs;
+using System;
 using System.Web.Http;
 
 namespace ShoppingApp
 {
     public class Startup
     {
+        private static readonly Lazy<JsonSerializer> JsonSerializerFactory = new Lazy<JsonSerializer>(GetJsonSerializer);
+
         public void Configuration(IAppBuilder app)
         {
             app.UseCors(CorsOptions.AllowAll);
@@ -17,6 +22,28 @@ namespace ShoppingApp
             app.UseWebApi(config);
 
             app.MapSignalR(new HubConfiguration { EnableDetailedErrors = true });
+
+            GlobalHost.DependencyResolver.Register(typeof(JsonSerializer), () => JsonSerializerFactory.Value);
+        }
+
+        private static JsonSerializer GetJsonSerializer()
+        {
+            return new JsonSerializer
+            {
+                ContractResolver = new FilteredCamelCasePropertyNamesContractResolver
+                {
+                    // 1) Register all types in specified assemblies:
+                    AssembliesToInclude =
+                 {
+                     typeof (Startup).Assembly
+                 },
+                    // 2) Register individual types:
+                    //TypesToInclude =
+                    //                {
+                    //                    typeof(Hubs.Message),
+                    //                }
+                }
+            };
         }
     }
 }

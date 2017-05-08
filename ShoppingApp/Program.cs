ad1babdfbcd1c815e4b2828591afa54921d972cc
Nick Owen <nowen@scottlogic.co.uk>
2017-05-08 15:51:57 +0100

    chore: move to OWIN console app

---
+++ ShoppingApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin.Hosting;
+using Serilog;
+using System;
+using System.Net.Http;
+
+namespace ShoppingApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .CreateLogger();
+                
+
+            string baseAddress = "http://localhost:52559/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+
+                Console.WriteLine($"Server is running on {baseAddress}");
+                Console.WriteLine("Press <enter> to stop server");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ShoppingApp/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ShoppingApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            appBuilder.UseWebApi(config);
+
+            //appBuilder.MapSignalR();
+        }
+    }
+}

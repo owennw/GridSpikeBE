1755fe794772ea1c2f4e543e77edd60a4291649a
Nick Owen <nowen@scottlogic.co.uk>
2017-05-09 16:19:09 +0100

    feat: add endpoint for searching customers by name

---
+++ ShoppingApp/Controllers/CustomerSearchController.cs
@@ -0,0 +1,39 @@
+﻿using ShoppingApp.Models;
+using ShoppingApp.Repositories;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoppingApp.Controllers
+{
+    public class CustomerSearchController : ApiController
+    {
+        private IRepository<Customer> repository;
+
+        public CustomerSearchController()
+        {
+            this.repository = new CustomerRepository();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Search()
+        {
+            var queryStringKeyValuePairs = this.Request.GetQueryNameValuePairs().ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            var nameSearch = queryStringKeyValuePairs["query"];
+
+            var customers = this.repository.GetAll()
+                .Distinct()
+                .Where(c => c.FirstName.Contains(nameSearch) || c.LastName.Contains(nameSearch));
+
+            if (customers.Any())
+            {
+                return Ok(customers);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
